cc7aea4d8ccac2c1d332f8aacfc3594b323efa27
spartan-longnguyen <long.nguyen@c0x12c.com>
2023-11-08 04:04:11 +0700

    aws-training-rds: add query to database

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-#
-# app = Flask(__name__)
-#
-# # Configure the database connection URL for Amazon RDS
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@longnguyen-db.ci1xxxcuy6pi.ap-southeast-1.rds.amazonaws.com:5432/postgres'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# # Initialize SQLAlchemy and Migrate
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-#
-# # Define your models below
-#
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#
-#     def __repr__(self):
-#         return f'<User {self.username}>'
-
-from flask import Flask
-from models import db
+from flask import Flask, jsonify
+from models import db, User
 
 app = Flask(__name__)
 
@@ -34,4 +10,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize SQLAlchemy
 db.init_app(app)
 
-# ... define routes and other application logic
+with app.app_context():
+    db.create_all()
+@app.route('/')
+def index():
+    try:
+        with app.app_context():
+            # Query a user from the database (replace with your actual query)
+            user = User.query.first()
+            if user:
+                return jsonify({'message': 'Database connection successful!', 'user': user.username})
+            else:
+                return jsonify({'message': 'No users found in the database.'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from .models import User  # Import your models from models.py file
+
+__all__ = ['User']
 
 db = SQLAlchemy()
