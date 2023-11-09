5c04c34dd8f753ee6b86925f6ff00927dce75e64
RedHoes <longpd02@gmail.com>
2023-11-09 14:12:45 +0700

    aws-training-rds: remove cmt from app

--- app.py
+++ app.py
@@ -10,12 +10,9 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 
-# Configure the database connection URL for Amazon RDS
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@longnguyen-db.ci1xxxcuy6pi.ap-southeast-1.rds.amazonaws.com:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize SQLAlchemy
 db.init_app(app)
 
 with app.app_context():
@@ -26,7 +23,6 @@ with app.app_context():
 def index():
     try:
         with app.app_context():
-            # Query a user from the database (replace with your actual query)
             user = User.query.first()
             if user:
                 return jsonify({'message': 'Database connection successful!', 'user': user.username})
@@ -40,10 +36,8 @@ def index():
 def get_user():
     try:
         with app.app_context():
-            # Query all users from the User table
             users = User.query.all()
 
-            # Convert the query result to a list of dictionaries for JSON serialization
             users_list = []
             for user in users:
                 users_list.append({
@@ -52,7 +46,6 @@ def get_user():
                     'email': user.email
                 })
 
-            # Return the list of users as JSON response
             return jsonify(users_list)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -62,12 +55,10 @@ def get_user():
 def add_user():
     try:
         with app.app_context():
-            # Parse data from the JSON request
             data = request.get_json()
             username = data.get('username')
             email = data.get('email')
 
-            # Create a new User object and add it to the database
             new_user = User(username=username, email=email)
             db.session.add(new_user)
             db.session.commit()
